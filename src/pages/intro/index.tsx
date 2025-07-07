14c395af5cb2c80f2eb99dd404e79ef70860cae7
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-07-07 11:44:29 -0300

    make intro page responsive

--- src/pages/intro/index.tsx
+++ src/pages/intro/index.tsx
@@ -1,5 +1,4 @@
 import { Logo } from '../../components/storyteller/logo';
-import HintExample from '../../../public/hint-example.jpg'
 import './styles.scss';
 import { FaHandPointRight } from 'react-icons/fa';
 
@@ -8,18 +7,19 @@ export const IntroPage = ({ setIsNewSession }: { setIsNewSession: React.Dispatch
 
     return (
         <div className='intro-page'>
-            <div className='title-bg' />
-            <div className='action-bg' />
-            <p className='subtitle'>Crie novas histórias com</p>
-            <Logo />
+            <div className='title-bg'>
+                <p className='subtitle'>Crie novas histórias com</p>
+                <Logo />
+            </div>
+
             <h2>O que é?</h2>
             <p>
-                O Storyteller é uma ferramenta criativa feita para quem quer <b>ganhar tempo na criação de conteúdo</b> e se conectar de forma mais empática com seu público. Ideal para tirar ideias do papel, <b>superar bloqueios criativos</b> e acompanhar o ritmo dinâmico das tendências.
+                O Storyteller é uma ferramenta de suporte criativo feita para quem quer <b>ganhar tempo na criação de conteúdo</b> e se conectar de forma mais empática com seu público. Ideal para tirar ideias do papel e <b>superar bloqueios criativos</b> sem deixar de acompanhar o ritmo dinâmico das tendências.
             </p>
 
             <h2>Como posso usar?</h2>
             <p>
-                Responda a 9 perguntas simples e receba roteiros e ideias criadas sob medida, com base nas suas respostas. Use para posts, vídeos, campanhas, narrativas ficcionais ou qualquer outro formato criativo.
+                Responda a <b>9 perguntas</b> simples e receba roteiros e ideias criadas sob medida, com base nas suas respostas. Use para posts, vídeos, campanhas, narrativas ficcionais ou qualquer outro formato criativo.
             </p>
 
             <h2>Para quem é?</h2>
@@ -27,21 +27,22 @@ export const IntroPage = ({ setIsNewSession }: { setIsNewSession: React.Dispatch
                 ✨ Para quem trabalha com criatividade – de <b>criadores de conteúdo</b> a profissionais de <b>marketing</b> e audiovisual.<br />
                 🛍️ Se você tem uma loja, conecte-se com os <b>assuntos em alta</b> e ganhe tempo na criação de posts.<br />
                 📱 Produz <b>vídeos para redes sociais</b>? Gere roteiros que engajam com seu público.<br />
-                🎬 Atua com <b>audiovisual</b>? Use como ponto de partida para desenvolver histórias completas.
+                🎬 Atua com <b>audiovisual</b>? Use como ponto de partida para desenvolver histórias completas.<br />
+                💡 Ou se você é apenas uma pessoa que gosta de <b>explorar novas histórias e narrativas</b>, o Storyteller também pra você!
             </p>
 
-            <h2>Fique atento às dicas</h2>
-            <p>Sempre a baixo das perguntas terão dicas como essa:</p>
-            <img src={HintExample} />
-            <p>Elas te darão um caminho para você pensar na resposta.</p>
+            <h2>Fique atento às dicas!</h2>
+            <p>Sempre a baixo das perguntas terão dicas que darão um caminho para você pensar na resposta.</p>
 
-            <div className='ready-text'>
-                <h2 ><b>Pronto para começar?</b></h2>
-                <p>
-                    Clique e desbloqueie novas narrativas
-                </p>
+            <div className='action-bg'>
+                <div className='ready-text'>
+                    <h2>Pronto para começar?</h2>
+                    <p>
+                        Clique e desbloqueie novas narrativas:
+                    </p>
+                </div>
+                <button onClick={start}>Começar <FaHandPointRight /></button>
             </div>
-            <button onClick={start}>Começar <FaHandPointRight /></button>
         </div>
     )
 }
