b5ef35f52ba146d60d510cf4676a504f30dd376d
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-07-14 16:27:29 -0300

    set lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,87 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  js.configs.recommended,
+  { ignores: ['dist', 'node_modules'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.scss', '.css']
+        }
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
+    extends: [
+      ...tseslint.configs.recommended,
+      js.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript
+    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
+
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'indent': ['error', 4],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'warn',
+
+      'react-hooks/exhaustive-deps': 'off',
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
-)
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'index',
+            'sibling'
+          ],
+          pathGroups: [
+            {
+              pattern: '*.json',
+              group: 'internal',
+              position: 'after'
+            },
+            {
+              pattern: '*.scss',
+              group: 'sibling',
+              position: 'after'
+            },
+            {
+              pattern: './*.scss',
+              group: 'sibling',
+              position: 'after'
+            }
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          },
+          'newlines-between': 'never'
+        }
+      ]
+    }
+  }
+);
