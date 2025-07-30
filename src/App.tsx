3a3c5776539b5cbadc74f129367f435199ac45eb
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-07-30 15:23:28 -0300

    apply vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import { useContext } from 'react';
 import { AnimatedBackground } from './components/animated-background';
 import { LangButton } from './components/lang-button';
@@ -20,6 +22,8 @@ const App = () => {
                     <MainPage />
                 }
             </div>
+            <Analytics />
+            <SpeedInsights />
         </div>
     );
 };
